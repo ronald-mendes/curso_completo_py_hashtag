39b3811e14b3392d9f9e41ec20386712a4685dfc
Ronald Mendes <ronald.mendes88@gmail.com>
2026-04-27 20:03:39 -0300

    Add files
    This is only from add files.

---
+++ main.py
@@ -0,0 +1,41 @@
+#importar o twilio
+
+# instalar:
+    # pandas
+    # openpyxl
+    # twilio
+
+import pandas as pd
+from twilio.rest import Client
+
+
+# Your Account SID and Auth Token from console.twilio.com
+account_sid = "account_sid"
+auth_token  = "auth_token"
+client = Client(account_sid, auth_token)
+
+
+# Abrir os 6 arquivos em excel
+
+lista_meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho"]
+
+
+for mes in lista_meses:
+    #print(mes)
+    tabela_vendas = pd.read_excel(f"{mes}.xlsx")
+    #print(tabela_vendas)
+    if (tabela_vendas["Vendas"] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas["Vendas"] > 55000, "Vendedor"].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas["Vendas"] > 55000, "Vendas"].values[0]
+        print(f"No mês {mes}, o {vendedor} vendeu {vendas}! Parabéns por batera meta!")
+        message = client.messages.create(
+            to="+5531xxxxxxx", # your number
+            from_="seu_numero_twillio",
+            body=f"No mês {mes}, o {vendedor} vendeu {vendas}! Parabéns por batera meta!")
+        print(message.sid)
+
+
+# Para cada arquivo:
+    # Verificar se algum valor na coluna vendas daquele arquivo é maior que 55.000
+    # Se for maior que 55.000 -> Envia um SMS com o Nome, o mês e as vendas do vendedor
+    # Caso não seja maior que 55.000 -> não fazer nada
